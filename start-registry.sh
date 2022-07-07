a9c2c6f12fa483cc121ba64e9b1c0fcd226fef49
Nikolay Alexeyev <n_alexeyev@hotmail.com>
2022-07-07 23:25:08 +0300

    Add external config for the registry
    Signed-off-by: Nikolay Alexeyev <n_alexeyev@hotmail.com>

--- start-registry.sh
+++ start-registry.sh
@@ -8,6 +8,5 @@ sleep 2
 docker-compose ls
 
 echo -e "\nBy default registry is available at https://wsl:5000 (check https://wsl:5000/v2/_catalog in your browser)"
-echo -e echo"and registry-ui is available at http://wsl:8081 (not https)\n"
+echo -e "and registry-ui is available at http://wsl:8081 (not https)\n"
 read -t5 -n 1 -s -r -p "Wait 5 seconds or press any key to finish...."
-echo -e "\n"
